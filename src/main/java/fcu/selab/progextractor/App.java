d54ee422fac519776e59ddeb295a0bd0f2432ea6
cofitost <zxcv091922@gmail.com>
2020-06-08 16:03:00 +0800

    first commit

---
+++ src/main/java/fcu/selab/progextractor/App.java
@@ -0,0 +1,13 @@
+package fcu.selab.progextractor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
